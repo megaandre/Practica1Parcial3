709ab5fb0eaa511d0d3cd750245ed3e3bb446020
Andre <cdmx2528@amerike.edu.mx>
2023-10-24 08:22:12 -0600

    Agregando Boostrap

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,12 @@
-let $btnLight = document.querySelector("#btn-light");
-let $btnDark = document.querySelector("#btn-dark");
+const toggleButton = document.getElementById("toggle-theme");
+const themeStyle = document.getElementById("theme-style");
 
+toggleButton.addEventListener("click", function() {
+    if (themeStyle.getAttribute("href") === "style.css") {
+        themeStyle.href = "styles-dark.css";
+        document.body.classList.add("dark-mode");
+    } else {
+        themeStyle.href = "style.css";
+        document.body.classList.remove("dark-mode");
+    }
+});
